0e68fa0d60817fe7ebc69113f1861485c4f44241
Dima Gaykov <dima.gaykov.04@gmail.com>
2021-11-26 19:58:28 +0300

    lab1

---
+++ LabOAP1/LabOAP1/LabOAP1.cpp
@@ -0,0 +1,32 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int A, B, C;
+	cin >> A;
+	cin >> B;
+	cin >> C;
+	if (A % 2 == 0 && B % 2 == 0) {
+		cout << "\nA и B четные";
+
+
+	}
+	else if (A % 2 == 0 && C % 2 == 0) {
+		cout << "\nA и C четный";
+	}
+	else if (B % 2 == 0 && C % 2 == 0) {
+		cout << "\nC и B четные";
+	}
+	else {
+		cout << "\nДвух четных нет!"; 
+	}
+}
+
